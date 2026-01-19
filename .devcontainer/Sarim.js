b05b811164207d95272e67c1350739b203172b82
Sarim-Ali-d <syedsarim386@gmail.com>
2026-01-19 06:45:36 +0000

    container config

---
+++ .devcontainer/Sarim.js
@@ -0,0 +1,14 @@
+/*const accountName = "Sarim"
+let accountEmail = "sar@386.com"
+var accountPass = 12123
+
+accountEmail= "Saif@321 "
+accountPass= "9876"
+let accountStatus;
+console.log(accountEmail);
+console.table([accountName, accountEmail, accountPass, accountStatus ]);
+*/
+let oneNo = 22
+let stringNo = String(oneNo)
+console.log(stringNo)
+console.log(typeof stringNo)
